93bd383a7155c618721c6860c797004df42a19d6
이도윤 <liftyun@gmail.com>
2025-08-07 17:22:05 +0900

    리프레시 오류 확인

--- back/src/main/java/com/dolijo/moring/member/valueobject/SocialType.java
+++ back/src/main/java/com/dolijo/moring/member/valueobject/SocialType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 /** 소셜 종류  */
 @Getter
 public enum SocialType {
-    GOOGLE("구글"), KAKAO("카카오"), NAVER("네이버"), APPLE("애플");
+    LOCAL("로컬"), GOOGLE("구글"), KAKAO("카카오"), NAVER("네이버"), APPLE("애플");
 
     private final String description;
     SocialType(String description) {
